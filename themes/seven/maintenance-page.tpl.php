128b59115cdd75edc9874dde06e965794678d71b
alttehn <31592612+alttehn@users.noreply.github.com>
2017-09-10 18:28:45 +0300

    5

--- themes/seven/maintenance-page.tpl.php
+++ themes/seven/maintenance-page.tpl.php
@@ -22,7 +22,7 @@
 	    <?php print '111111111111'; ?>
 		     <?php print getenv("MYSQL_USER"); ?>	   <?php print '2'; ?>
       <?php print  getenv("MYSQL_PASSWORD"); ?>	   <?php print '2'; ?>
-      <?php print  getenv("MYSQL_SERVICE_HOST"); ?>	   <?php print '2'; ?>
+      <?php print  getenv("LOGIN_SERVICE_HOST"); ?>	   <?php print '2'; ?>
       <?php print  getenv("LOGIN_SERVICE_PORT"); ?>
 	   <?php print '2'; ?>
 	 <?php print  getenv("MYSQL_USER"); ?>
